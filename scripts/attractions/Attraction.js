8af926c0c1c49c1b42c49f3d1be0b1cfd15f8637
j0verton <overtonje@gmail.com>
2020-09-09 16:23:31 -0400

    merged

--- scripts/attractions/Attraction.js
+++ scripts/attractions/Attraction.js
@@ -6,27 +6,3 @@ export const AttractionPreviewHTML = (attraction) => {
     <button type="button" class="attraction detailsBtn">Details</button>
     `
 }
-
-export const AttractionDetailsHTML = (attraction) => {
-
-    <div>${attraction.description}</div> 
-    ${amenetiesLogic(attraction)}
-    `
-}
-// checks the attraction object to see if the ameneties are listed as true or false 
-// and creates HTML to reflect that
-const amenetiesLogic = (attraction) => {
-        console.log(attraction)
-        let amenetiesHTML = ``
-        if (attraction.ameneties.souvenirs === true) {
-        amenetiesHTML += `<div>✔️ Giftshop</div>`
-    } else {
-        `<div>❌ Giftshop</div>`
-    }
-    if (attraction.ameneties.restrooms === true) {
-        amenetiesHTML += `<div>✔️Restrooms</div>`
-    } else {
-        `<div>❌ Restrooms</div>`
-    }
- return amenetiesHTML
-}
