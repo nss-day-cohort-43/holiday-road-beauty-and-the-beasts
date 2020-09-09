ab926c27580a4ac5cc0016d7298fd9a1ae340059
j0verton <overtonje@gmail.com>
2020-09-09 16:08:31 -0400

    working on details button

--- scripts/attractions/Attraction.js
+++ scripts/attractions/Attraction.js
@@ -3,23 +3,30 @@ export const AttractionPreviewHTML = (attraction) => {
     return `
     <div>${attraction.name}</div>
     <div>${attraction.city}, ${attraction.state}</div>
+    <button type="button" class="attraction detailsBtn">Details</button>
     `
 }
 
+export const AttractionDetailsHTML = (attraction) => {
+
+    <div>${attraction.description}</div> 
+    ${amenetiesLogic(attraction)}
+    `
+}
 // checks the attraction object to see if the ameneties are listed as true or false 
 // and creates HTML to reflect that
-// const amenetiesLogic = (attraction) => {
-    //     console.log(attraction)
-    //     let amenetiesHTML = ``
-    //     if (attraction.ameneties.souvenirs === true) {
-//         amenetiesHTML += `<div>✔️ Giftshop</div>`
-//     } else {
-//         `<div>❌ Giftshop</div>`
-//     }
-//     if (attraction.ameneties.restrooms === true) {
-//         amenetiesHTML += `<div>✔️Restrooms</div>`
-//     } else {
-//         `<div>❌ Restrooms</div>`
-//     }
-//  return amenetiesHTML
-// }
+const amenetiesLogic = (attraction) => {
+        console.log(attraction)
+        let amenetiesHTML = ``
+        if (attraction.ameneties.souvenirs === true) {
+        amenetiesHTML += `<div>✔️ Giftshop</div>`
+    } else {
+        `<div>❌ Giftshop</div>`
+    }
+    if (attraction.ameneties.restrooms === true) {
+        amenetiesHTML += `<div>✔️Restrooms</div>`
+    } else {
+        `<div>❌ Restrooms</div>`
+    }
+ return amenetiesHTML
+}
