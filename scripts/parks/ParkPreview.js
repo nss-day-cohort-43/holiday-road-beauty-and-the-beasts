b878e8ff5f5b1b1dd8a19f1c3ccdbf5c34898fe4
Megan McCoy <mnmccoy95@gmail.com>
2020-09-09 15:32:24 -0500

    working on details button

--- scripts/parks/ParkPreview.js
+++ scripts/parks/ParkPreview.js
@@ -9,6 +9,11 @@ export const mainListener = () => {
 const eventHub = document.querySelector("main")
 //defines where park info will be sent on page
 const parkPreviewTarget = document.querySelector(".preview-park")
+//defines where details of park will go on page
+const detailsTarget = document.querySelector(".park-detail-container")
+//holds parks that was chosen after event
+let parkArray = []
+
 
 //listens for a change from park dropdown
 eventHub.addEventListener("parkChosen", event => {
@@ -25,6 +30,8 @@ eventHub.addEventListener("parkChosen", event => {
                 return false
             }
         })
+        //places matching park in park array
+        parkArray = matchingPark
         //renders HTML of matching park
         renderParkPreview(matchingPark)
     }
@@ -40,10 +47,24 @@ const renderParkPreview = (parkChosen) => {
                     <a href="${parkObj.url}" target="_blank" class="park-site">Park Website</a>
                     <div class="location">${parkObj.addresses[1].city}, ${parkObj.addresses[1].stateCode}</div>
                     <button id="parkDetails">Details</button>
+                    <div class="park-detail-container"</div>
                     `
                 })
             }
         </div>
-    
     `
+}
+
+eventHub.addEventListener("click", clickEvent => {
+    if(clickEvent.target.id === "parkDetails"){
+        renderParkDetails(parkArray)
+    }
+})
+
+
+const renderParkDetails = (parkChosen) => {
+    detailsTarget.innerHTML = `
+    <div class
+    `
+
 }
