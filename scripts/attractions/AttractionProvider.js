02cbc68b83ddc75ca78f347f79886ce45b921e9b
j0verton <overtonje@gmail.com>
2020-09-09 15:14:33 -0400

    wrote comments for my code, actually

--- scripts/attractions/AttractionProvider.js
+++ scripts/attractions/AttractionProvider.js
@@ -1,11 +1,13 @@
 //this is a container for holding the attractions from the API 
 let attractions =[]
 
-//creates a copy of the array of attractions that other modules can use
+//creates a copy of the array of attractions that other modules can use without changing the fetched data
 export const useAttractions = () => {
     return attractions.slice();
 }
 
+/*fetches the array of attraction objects from the API, 
+then converts it to a Javascript object and saves it to the attractions array */
 export const getAttractions = () =>  {
     return fetch ("http://holidayroad.nss.team/bizarreries")
     .then(response => response.json())
