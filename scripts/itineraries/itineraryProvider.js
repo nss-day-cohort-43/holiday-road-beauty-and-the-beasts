942d42aaf131f4d2899c5648801d40d9b6b789d7
j0verton <overtonje@gmail.com>
2020-09-10 15:22:08 -0400

    build a save itin button that appears when 3 options are selected

---
+++ scripts/itineraries/itineraryProvider.js
@@ -0,0 +1,25 @@
+
+const eventHub = document.querySelector("main")
+
+// an event listener to tell us when all 3 options have been chosen, 
+// if they have it generates a 'save itinerary' button
+// button will disappear if you return a dropdown to zerp\o
+eventHub.addEventListener ("change", event => {
+    
+    const selectedPark = document.querySelector("#parkSelect")
+    const selectedEatery = document.querySelector("#eaterySelector")
+    const selectedAttraction = document.querySelector("#attractionSelect")
+    const saveBtnContainer = document.getElementById("saveBtnContainer")
+
+    console.log(selectedAttraction.value) 
+    if (selectedPark.value !== "0" &&
+     selectedEatery.value !== "0" && 
+     selectedAttraction.value !== "0") {
+        saveBtnContainer.innerHTML = `<button type="button" id="saveBtn">Save Itinerary</button>`
+    } else {
+        saveBtnContainer.innerHTML = ""
+    }
+} )
+
+
+export const saveItinerary = ()=>{}
--- scripts/main.js
+++ scripts/main.js
@@ -5,6 +5,7 @@ import { AttractionSelect } from './attractions/AttractionSelector.js';
 import { attractionsPreviewListener } from './attractions/AttractionsPreview.js';
 import { parkPreviewListener } from './parks/ParkPreview.js';
 import { mainListenerForEatery } from './eateries/EateryPreview.js';
+import {saveItinerary} from './itineraries/itineraryProvider.js'
 
 // This is for the mainListener calls
 mainListenerForEatery();
