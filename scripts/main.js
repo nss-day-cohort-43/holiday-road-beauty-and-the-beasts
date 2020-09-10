f8021acb010cdb11222b5502ea55f5c79536173d
Parker Kelley <Pklly92@gmail.com>
2020-09-10 16:03:18 -0500

    We're done with the first version of the working details button.

--- scripts/main.js
+++ scripts/main.js
@@ -9,19 +9,11 @@ import { saveItinerary } from './itineraries/itineraryProvider.js';
 
 // This is for the mainListener calls
 eateryPreviewListener();
-//allows main to access the listener
 parkPreviewListener();
 attractionsPreviewListener();
 parkListener();
 // This is the end of the mainListener calls
 
-//lets the user know that the parks are loading
-// loadingParks();
-//everything dealing with park data MUST be after getParks() bc it takes FOREVER
-getParks().then(() => {
-	ParkSelect();
-	//add anything relating to a park HERE or else it won't run
-});
 stateSelect();
 
 eateryDropdownRender();
