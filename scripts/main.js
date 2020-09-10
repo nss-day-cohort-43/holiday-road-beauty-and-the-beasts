de421658cf65b1e6b6935fb60f6e4383eda39f29
Megan McCoy <mnmccoy95@gmail.com>
2020-09-10 15:11:09 -0500

    finished state drop down and corresponding parks render

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,5 @@
-import { ParkSelect, stateSelect } from './parks/ParkList.js';
-import { getParks, loadingParks } from './parks/ParkProvider.js';
+import { stateSelect } from './parks/ParkList.js';
+import { parkListener } from './parks/ParkProvider.js';
 import { eateryDropdownRender } from './eateries/EateryList.js';
 import { AttractionSelect } from './attractions/AttractionSelector.js';
 import { attractionsPreviewListener } from './attractions/AttractionsPreview.js';
@@ -9,19 +9,12 @@ import {saveItinerary} from './itineraries/itineraryProvider.js'
 
 // This is for the mainListener calls
 mainListenerForEatery();
-//allows main to access the listener
 parkPreviewListener();
 attractionsPreviewListener();
+parkListener();
 // This is the end of the mainListener calls
 
 stateSelect()
-//lets the user know that the parks are loading
-loadingParks()
-//everything dealing with park data MUST be after getParks() bc it takes FOREVER
-getParks().then(() => {
-	ParkSelect();
-	//add anything relating to a park HERE or else it won't run
-});
 
 eateryDropdownRender();
 
--- scripts/parks/ParkList.js
+++ scripts/parks/ParkList.js
@@ -1,13 +1,21 @@
 import { useParks } from './ParkProvider.js'
 
+//defines the eventHub
+const eventHub = document.querySelector("main")
+
+
 //state abbreviations array
 const states = [ 'AL', 'AK', 'AS', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'DC', 'FM', 'FL', 'GA', 'GU', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MH', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'MP', 'OH', 'OK', 'OR', 'PW', 'PA', 'PR', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VI', 'VA', 'WA', 'WV', 'WI', 'WY' ];
 
 //targets where to put state dropdown
 const stateTarget = document.querySelector(".state")
+
 //create drop down to chose state
 export const stateSelect = () => {
-    stateTarget.innerHTML = `<select class="dropdown" id="stateSelect"></select>`
+    stateTarget.innerHTML = `
+        <select class="dropdown" id="stateSelect">
+            <option value="0">Choose a State!</option>
+        </select>`
     const selectTarget = document.querySelector("#stateSelect")
     let stateSelectHTML = ""
     for (const place of states) {
@@ -23,15 +31,28 @@ const renderStates = (stateObj) => {
     return `<option value="${stateObj}">${stateObj}</option>`
 }
 
+//adds an event listener for when state is chosen
+eventHub.addEventListener("change", event => {
+    if(event.target.id ==="stateSelect"){
+        const stateValue = event.target.value
+        const stateChosenEvent = new CustomEvent("stateChosen", {
+            detail: {
+                stateWanted: stateValue
+            }
+        })
+        eventHub.dispatchEvent(stateChosenEvent)
+    }
+})
+
 
 //targets html location for park select
 const parkTarget = document.querySelector(".park")
 
 //initial render of parks dropdown
 export const ParkSelect = () => {
-            const parkList = useParks()
-            render(parkList)  
-        }
+        const parkList = useParks()
+        render(parkList)  
+}
 
 //inner HTML of dropdown
 const render = (parkCollection) => {
@@ -48,8 +69,7 @@ const render = (parkCollection) => {
     `
 }
 
-const eventHub = document.querySelector("main")
-
+//defines custom event for when park is chosen
 eventHub.addEventListener("change", event => {
     if (event.target.id === "parkSelect"){
         const parkId = event.target.value
