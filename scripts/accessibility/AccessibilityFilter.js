03db85b6ff1ac72eee7bea7afb2902ea39241ad2
j0verton <overtonje@gmail.com>
2020-09-14 13:39:35 -0500

    commented and cleaned up code for pull

--- scripts/accessibility/AccessibilityFilter.js
+++ scripts/accessibility/AccessibilityFilter.js
@@ -3,61 +3,27 @@ import { useAllAccessibility } from "./AccessibilityProvider.js";
 
 const eventHub = document.querySelector("main")
 
-
+//currently list function just serves to connect the module to main
 export const addAccessibileParkIcons = () => {
-    
     let allAccessibleParks = useAllAccessibility()
     let parks = useParks()
-
 }
 
+//this is an event listener which adds accessibility emojis to any park on the dropdown list with accessibility features
 eventHub.addEventListener('click', (event) => {
     if (event.target.id === "parkSelect") {
-    
-    let allAccessibleParks = useAllAccessibility()
-    console.log(allAccessibleParks)
-    let parkDiv = document.querySelector(".park")
-    let parkDropdown = document.querySelector("#parkSelect") 
-    let parkOptions = parkDropdown.querySelectorAll('option')
-    let categories = allAccessibleParks.flat(2)
-    // console.log(bigString)
-    parkOptions.forEach(element => {
-        categories.forEach(category => {
-            category.parks.forEach(park => {
-                if (element.value === park.fullName)
-                {element.innerHTML += "♿"}
-            })
+        let allAccessibleParks = useAllAccessibility()
+        let parkDropdown = document.querySelector("#parkSelect") 
+        let parkOptions = parkDropdown.querySelectorAll('option')
+        let categories = allAccessibleParks.flat(2)
+        // console.log(bigString)
+        parkOptions.forEach(element => {
+            categories.forEach(category => {
+                category.parks.forEach(park => {
+                    if (element.value === park.fullName)
+                    {element.innerHTML += "♿"}
+                })
+            })        
         })
-
-        // console.log(element)
-        
-    })
-}
+    }
 })
-
-
-    
-
-                // console.table(parks.data)
-                // accesibleParks = parks.data.forEach(array => {
-                //     array[0]
-                // })
-                // console.log(accesibleParks)
-
-
-
-                // parks.data.map(park => {
-                //     if (park.categories[0] === "Accessibility"){ 
-                //         let parkDropdown = document.querySelector(".park")
-                //         // debugger
-                //         let parkOptions = parkDropdown.querySelectorAll("option")
-                //         // console.log("parkOptions",parkOptions )
-                //         parkOptions.forEach(option => {
-                //             if (option.value === park.id) {
-                //                 console.log("match", park.name)
-                //                 option.innerHTML += "♿"
-                //             }
-                //         } )
-                //     }
-                // })
-            
--- scripts/accessibility/AccessibilityProvider.js
+++ scripts/accessibility/AccessibilityProvider.js
@@ -5,26 +5,18 @@ let parksGeneral = []
 let parksMobility = []
 let parksVision = []
 let parksHearing = []
-
 let parksAccessibilityAll =[]
 
-let accesibleParks
-let accessibleRoomParksArray
-let accessibleSitesParksArray
-let accessibleRestroomsParksArray
-let accessibleParkParksArray
-// useParks() 
-
-const eventHub = document.querySelector("main")
-
+//creates a usable array of accessibility data on other pages
 export const useAllAccessibility = () => {
     let allAccessibility = []
     allAccessibility.push(parksGeneral, parksMobility, parksVision, parksHearing)
-    console.log(allAccessibility)
     parksAccessibilityAll = allAccessibility.flat()
     return parksAccessibilityAll
 }
 
+
+//fetches all the accessibility data from the NPS API and stores it it 4 variables by category
 export const fetchAccessInfo = () => {
 
     allAccessibility.hearing.forEach(key => { 
@@ -36,8 +28,6 @@ export const fetchAccessInfo = () => {
             parsedParks => {
                 if (parsedParks.data[0]) {
                 parksHearing.push(parsedParks.data[0][0])
-                } else {
-                    console.log(0)
                 }
         }))     
     })
@@ -77,109 +67,36 @@ export const fetchAccessInfo = () => {
  
 }
 
-        const allAccessibility = {
-            "hearing": [
-                "Assistive Listening System", 
-                "Assistive Listening System - T-Coil Compatible", 
-                "Audio Description", 
-                "Audio Description - Live",
-                "Captioned Media",
-                "Open Captioning"
-            ],
-            "vision": [
-                "Braille",
-                "Large Print",
-                "Low-Vision Access",
-                "Tactile Exhibit",
-            ],
-            "mobility": [
-                "Accessible Rooms",
-                "Accessible Sites",
-                "Automated Entrance",
-                "Benches/Seating",
-                "Elevator",
-                "Restroom - Accessible",
-                "Wheelchair Accessible",
-                "Wheelchairs Available",
-            ],
-            "general": [
-                "Animal-Safe Food Storage",
-                "Bus/Shuttle Stop",
-                "Electrical Outlet/Cell Phone Charging",
-                "First Aid/Medical Care Available",
-            ]
-        }
-
-
-
-    // fetch(`https://developer.nps.gov/api/v1/amenities/parksplaces/?q=Assistive&api_key=${defaultExport.npsKey}`)
-    //             .then(response => response.json()
-    //             .then(
-    //             parsedParks => {
-    //                 parksAccessi = parsedParks
-
-    //                 console.log(parksAccessi)
-    //             }))
-
-
- 
-
-
-
-
-
-
-
-
-// export const fetchAccessInfo = () => {
-//         fetch(`https://developer.nps.gov/api/v1/amenities/parksplaces/?q=accessi&api_key=4CCbkNphNpJ3QSNIMKkkLeLl2DrSerPMI1iTBQl1`)
-//             .then(response => response.json()
-//             .then(
-//             parsedParks => {
-//                 parksAccessi = parsedParks
-//             }))
-//         .then( fetch(`https://developer.nps.gov/api/v1/amenities/parksplaces/?q=animal&api_key=4CCbkNphNpJ3QSNIMKkkLeLl2DrSerPMI1iTBQl1`)
-//             .then(response => response.json()
-//             .then(
-//                 parsedParks => {
-//                     parksAnimal = parsedParks
-//                 })))
-//         .then(fetch(`https://developer.nps.gov/api/v1/amenities/parksplaces/?q=assistive&api_key=4CCbkNphNpJ3QSNIMKkkLeLl2DrSerPMI1iTBQl1`)
-//             .then(response => response.json()
-//             .then(
-//                 parsedParks => {
-//                     parksAssistive = parsedParks
-//             })))
-//         .then(fetch(`https://developer.nps.gov/api/v1/amenities/parksplaces/?q=audio&api_key=4CCbkNphNpJ3QSNIMKkkLeLl2DrSerPMI1iTBQl1`)
-//             .then(response => response.json()
-//             .then(
-//             parsedParks => {
-//                 parksAudio = parsedParks
-//             })))
-//         .then(fetch(`https://developer.nps.gov/api/v1/amenities/parksplaces/?q=automated&api_key=4CCbkNphNpJ3QSNIMKkkLeLl2DrSerPMI1iTBQl1`)
-//             .then(response => response.json()
-//             .then(
-//                 parsedParks => {
-//                     parksAutomated = parsedParks
-//             })))
-//         .then (fetch(`https://developer.nps.gov/api/v1/amenities/parksplaces/?q=benches&api_key=4CCbkNphNpJ3QSNIMKkkLeLl2DrSerPMI1iTBQl1`)
-//             .then(response => response.json()
-//             .then(
-//                 parsedParks => {
-//                     parksBenches = parsedParks
-//             })))
-//             debugger
-// }
-
-        
-
-
-
-
-
-
-        
-        
-        // .then(ParkSelect)
-    
-
+//an array of all the accessibility features in the NPS api
+const allAccessibility = {
+    "hearing": [
+        "Assistive Listening System", 
+        "Assistive Listening System - T-Coil Compatible", 
+        "Audio Description", 
+        "Audio Description - Live",
+        "Captioned Media",
+        "Open Captioning"
+    ],
+    "vision": [
+        "Braille",
+        "Large Print",
+        "Low-Vision Access",
+        "Tactile Exhibit",
+    ],
+    "mobility": [
+        "Accessible Rooms",
+        "Accessible Sites",
+        "Automated Entrance",
+        "Benches/Seating",
+        "Elevator",
+        "Restroom - Accessible",
+        "Wheelchair Accessible",
+        "Wheelchairs Available",
+    ],
+    "general": [
+        "Animal-Safe Food Storage",
+        "Bus/Shuttle Stop",
+        "Electrical Outlet/Cell Phone Charging",
+        "First Aid/Medical Care Available",
+    ]
+}
