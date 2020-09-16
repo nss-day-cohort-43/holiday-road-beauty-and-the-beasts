203e6a8f6ecfd298d6ee1044dbf9e7a7f59b969f
j0verton <overtonje@gmail.com>
2020-09-16 10:55:28 -0500

    getting routing data back

--- scripts/directions/DirectionList.js
+++ scripts/directions/DirectionList.js
@@ -1,13 +1,22 @@
 import { geocodeLocations, getRoute } from "./DirectionProvider.js"
 
 const eventHub = document.querySelector("main")
+
 let routeData
+
 eventHub.addEventListener("routeBtnClicked", event => {
-    let locationsData = geocodeLocations(event.detail)
-    console.log(locationsData)
-        routeData = getRoute(locationsData)
-        console.log(routeData)
-    
+    geocodeLocations(event.detail).then((response) => {
+        
+        let locationsData = response
+        getRoute(locationsData)
+        .then(response => {
+            routeData = response
+            console.log(routeData)
+            return routeData
+        })
+    })
 })
 
 export const directionListener = () => {}
+
+const renderRouteText = () => {}
--- scripts/directions/DirectionProvider.js
+++ scripts/directions/DirectionProvider.js
@@ -2,41 +2,43 @@ import defaultExport from '../Settings.js';
 
 
 let route
+let locationData = []
 
 export const geocodeLocations = (itinerary) => {
-	let locationData =[]
 	let locations = Object.values(itinerary)
 	locations.pop()
 	locations.unshift('nashville')
-	locations.forEach(location =>{
-		fetch (`https://graphhopper.com/api/1/geocode?q=${location}&locale=us&debug=true&key=${defaultExport.graphhopperKey}`)
+	const requests = locations.map(location => {
+		return fetch(`https://graphhopper.com/api/1/geocode?q=${location}&locale=us&debug=true&key=${defaultExport.graphhopperKey}`)
 			.then((response) => response.json())
-				.then((parsedlocation) => {
-					locationData.push(parsedlocation);
-				})
-			})
-			return locationData
+	})
+	return Promise.all(requests).then(response =>{
+		console.log(response)
+		return response
+	})
 }
 
 
 export const getRoute = (locationsData) => {
 	// debugger;
 	console.log(locationsData)
-	console.log(locationsData[0])
+	console.log(locationsData[0].hits[0])
 	// console.log(Object.values(locationsData))
 	// let location1 = locationsData.pop()
 	// console.log(location1)
-	
-	// let location1= locationsData[0].hits[0].points
-	let location2= locationsData[1].hits[0].points
-	let location3= locationsData[2].hits[0].points
-	let location4= locationsData[3].hits[0].points
 
-	return fetch (`https://graphhopper.com/api/1/route?point=${location1.lat},${location1.lng}&point=${location2.lat},${location2.lng}&point=${location3.lat},${location3.lng}&point=${location4.lat},${location4.lng}&vehicle=car&locale=us&instructions=true&calc_points=true&key=${defaultExport.graphhopperKey}`)
-	.then((response) => response.json())
-	.then((parsedRoute) => {
-		route = parsedRoute
-	})
+	let location1= locationsData[0].hits[0].point
+	let location2 = locationsData[1].hits[0].point
+	let location3 = locationsData[2].hits[0].point
+	let location4 = locationsData[3].hits[0].point
+
+	return fetch(`https://graphhopper.com/api/1/route?point=${location1.lat},${location1.lng}&point=${location2.lat},${location2.lng}&point=${location3.lat},${location3.lng}&point=${location4.lat},${location4.lng}&vehicle=car&locale=us&instructions=true&calc_points=true&key=${defaultExport.graphhopperKey}`)
+		.then((response) => response.json())
+		.then((parsedRoute) => {
+			route = parsedRoute
+			console.log(route)
+			return route
+		})
 }
 
 
@@ -53,20 +55,20 @@ Then all 4 lat/long pairs should be in the URL for the request to the Routing AP
 
 The step-by-step instructions in the response from the Routing API should be displayed below the Itinerary Preview section.
 
-	
+
 	<button type=button id="routeDisplay--${Itinerary.id}">Get Route</button>
 
 eventHub.addEventListener("clicks", (event) => {
-    if(event.target.id.contains("routeDisplay")){
-        console.log("route clicked")
-        const [prefix, routeNum] = event.target.id.split("--")
-        const routeEvent = new CustomEvent("routeBtnClicked", {
-            detail: {
-                routeId: routeNum
-            }
-        })
-        eventHub.dispatchEvent(routeEvent)
-    }
+	if(event.target.id.contains("routeDisplay")){
+		console.log("route clicked")
+		const [prefix, routeNum] = event.target.id.split("--")
+		const routeEvent = new CustomEvent("routeBtnClicked", {
+			detail: {
+				routeId: routeNum
+			}
+		})
+		eventHub.dispatchEvent(routeEvent)
+	}
 })
 
 	*/
